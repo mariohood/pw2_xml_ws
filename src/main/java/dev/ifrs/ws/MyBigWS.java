dd5b50c1a63dd78c55291ea54c43cc611941ee5f
mariohood <manhattan199@hotmail.com>
2022-05-31 15:37:33 -0300

    MyBigWS.java

--- src/main/java/dev/ifrs/ws/MyBigWS.java
+++ src/main/java/dev/ifrs/ws/MyBigWS.java
@@ -1,8 +1,14 @@
 package dev.ifrs.ws;
 
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService (name = "Big", serviceName = "Big")
 public class MyBigWS {
+    @WebMethod
+    public int convert(int a){
+
+        return a;
+    }
     
 }
